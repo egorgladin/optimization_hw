e9dd643ea4d9601f18289690820de44f011749a8
Egor Gladin <gladin.el@phystech.edu>
2021-02-07 18:56:01 +0300

    Implemented coordinate descent, conducted experiments

---
+++ hw1/coordinate_descent.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from hw1_nelder_mead import bird_function, plot_bird_function
+import random
+
+
+def bisection(g, y0, step, n_steps):
+    a, b = y0 - step, y0 + step
+    c = (a + b) / 2
+    gc = g(c)
+    for i in range(n_steps):
+        d = (a + c) / 2
+        e = (c + b) / 2
+        gd, ge = g(d), g(e)
+        if gd < gc:
+            c = d
+            b = c
+            gc = gd
+        elif ge < gc:
+            a = c
+            c = e
+            gc = ge
+        else:
+            a = d
+            b = e
+    return c
+
+
+def coordinate_descent(x0, step, f, n_bisection, tol):
+    x = x0
+    trajectory = [x.copy()]
+    n = len(x0)
+    iter = 0
+    stationary = 0
+    evals = [0]
+
+    while stationary < 2 * n:
+        random.seed(iter)
+        i = random.randint(0, 1)
+        iter += 1
+
+        def g(y):
+            x_ = x.copy()
+            x_[i] = y
+            return f(x_)
+
+        y = bisection(g, x[i], step, n_bisection)  # 1 + 2*n_bisection evals
+        if abs(x[i] - y) < tol:
+            stationary += 1
+        else:
+            stationary = 0
+        x[i] = y
+        trajectory.append(x.copy())
+        evals.append(evals[-1] + 1 + 2*n_bisection)
+
+    return x, trajectory, evals
+
+
+def main():
+    EXPERIMENT_NUMBER = 1
+
+    # initialize
+    if EXPERIMENT_NUMBER == 1:
+        x0 = -6 * np.ones(2)  # np.array([-5, -7])
+
+    elif EXPERIMENT_NUMBER == 2:
+        x0 = -4 * np.ones(2)
+
+    elif EXPERIMENT_NUMBER == 3:
+        x0 = -6 * np.ones(2)
+
+    else:
+        raise ValueError("Unknown experiment number")
+
+    # run coordinate descent
+    tol = 1e-2
+    n_bisection = 6
+    step = 3.
+    x_opt, trajectory, evals = coordinate_descent(x0, step, bird_function, n_bisection, tol)
+
+    plot_bird_function()
+
+    # plot trajectory
+    xs = [x[0] for x in trajectory]
+    ys = [x[1] for x in trajectory]
+    plt.plot(xs, ys, color='r', linewidth=1)
+    plt.savefig(f"experiment{EXPERIMENT_NUMBER + 3}.png", bbox_inches='tight')
+
+    fsize = 15
+    fig = plt.figure(figsize=(10, 5))
+    plt.plot(evals, [bird_function(x) for x in trajectory])
+    plt.xticks(fontsize=fsize)
+    plt.yticks(fontsize=fsize)
+    plt.xlabel(r"# of $f$ evaluations", fontsize=fsize)
+    plt.ylabel(r"$f(x)$", fontsize=fsize)
+    plt.savefig(f"experiment{EXPERIMENT_NUMBER + 3}_evals.png", bbox_inches='tight')
+    print(x_opt)
+
+
+if __name__=="__main__":
+    main()
--- hw1/nelder_mead.py
+++ hw1/nelder_mead.py
@@ -10,6 +10,7 @@ def nelder_mead(X0, f, tol, params):
     X = X0
     F = [f(x) for x in X]
     trajectory = []
+    evals = [len(X)]
 
     while stdev(F) > tol:
 
@@ -23,6 +24,7 @@ def nelder_mead(X0, f, tol, params):
         # reflection
         x_r = x_o + alpha * (x_o - X[-1])
         f_r = f(x_r)
+        evals.append(evals[-1] + 1)
         if F[0] <= f_r < F[-2]:
             X[-1] = x_r
             F[-1] = f_r
@@ -32,6 +34,7 @@ def nelder_mead(X0, f, tol, params):
         if f_r < F[0]:
             x_e = x_o + gamma * (x_r - x_o)
             f_e = f(x_e)
+            evals[-1] += 1
             if f_e < f_r:
                 X[-1] = x_e
                 F[-1] = f_e
@@ -43,19 +46,21 @@ def nelder_mead(X0, f, tol, params):
         # contraction
         x_c = x_o + rho * (X[-1] - x_o)
         f_c = f(x_c)
+        evals[-1] += 1
         if f_c < F[-1]:
             X[-1] = x_c
             F[-1] = f_c
             continue
 
         # shrink
-        for i in range(len(X) - 1):
-            X[i + 1] = X[0] + sigma * (X[i + 1] - X[0])
-            F[i + 1] = f(X[i + 1])
+        evals[-1] += len(X) - 1
+        for i in range(1, len(X)):
+            X[i] = X[0] + sigma * (X[i] - X[0])
+            F[i] = f(X[i])
 
     F, X = zip(*sorted(zip(F, X)))
     trajectory.append(deepcopy(X))
-    return X[0], F[0], trajectory
+    return X[0], F[0], trajectory, evals
 
 
 def bird_function(x):
@@ -77,6 +82,21 @@ def bird_function_array(X, Y):
     return Z
 
 
+def plot_bird_function():
+    # create a grid and choose function's levels to plot
+    X, Y = np.meshgrid(np.linspace(-10, 0, 100), np.linspace(-10, 0, 100))
+    Z = bird_function_array(X, Y)
+    vals = set(list(Z.flatten()))
+    vals.remove(1e12)
+    vals = list(vals)
+    min_, max_ = min(vals), max(vals)
+    labels = [min_ + (max_ - min_) * i / 10 for i in range(1, 10)]
+
+    # plot function
+    fig, ax = plt.subplots(figsize=(10, 10))
+    cs = ax.contour(X, Y, Z, labels)
+
+
 def init_simplex(x, step):
     X = [x]
     for i in range(len(x)):
@@ -87,22 +107,34 @@ def init_simplex(x, step):
 
 
 def main():
-    x0 = -6 * np.ones(2)
-    X0 = init_simplex(x0, 3)
-    params = {'alpha': 1., 'gamma': 2., 'rho': 0.5, 'sigma': 0.5}
-    x_opt, f_opt, trajectory = nelder_mead(X0, bird_function, 1e-2, params)
+    EXPERIMENT_NUMBER = 2
 
-    X, Y = np.meshgrid(np.linspace(-10, 0, 100), np.linspace(-10, 0, 100))
-    Z = bird_function_array(X, Y)
-    vals = set(list(Z.flatten()))
-    vals.remove(1e12)
-    vals = list(vals)
-    min_, max_ = min(vals), max(vals)
-    labels = [min_ + (max_ - min_) * i / 10 for i in range(1, 10)]
+    # initialize
+    if EXPERIMENT_NUMBER == 1:
+        x0 = -6 * np.ones(2)
+        X0 = init_simplex(x0, 3)
+        params = {'alpha': 1., 'gamma': 2., 'rho': 0.5, 'sigma': 0.5}
 
-    fig, ax = plt.subplots(figsize=(10, 10))
-    cs = ax.contour(X, Y, Z, labels)
+    elif EXPERIMENT_NUMBER == 2:
+        x0 = -4 * np.ones(2)
+        X0 = init_simplex(x0, 2)
+        params = {'alpha': 1., 'gamma': 2., 'rho': 0.5, 'sigma': 0.5}
 
+    elif EXPERIMENT_NUMBER == 3:
+        x0 = -6 * np.ones(2)
+        X0 = init_simplex(x0, 3)
+        params = {'alpha': 3., 'gamma': 4., 'rho': 0.5, 'sigma': 0.5}
+
+    else:
+        raise ValueError("Unknown experiment number")
+
+    # run Nelder-Mead method
+    tol = 1e-2
+    x_opt, f_opt, trajectory, evals = nelder_mead(X0, bird_function, tol, params)
+
+    plot_bird_function()
+
+    # plot triangles
     for X in trajectory:
         xs = [x[0] for x in X]
         ys = [x[1] for x in X]
@@ -110,7 +142,8 @@ def main():
         ys.append(ys[0])
         plt.plot(xs, ys, color='r', linewidth=1)
 
-    plt.savefig("levels.png", bbox_inches='tight')
+    plt.savefig(f"experiment{EXPERIMENT_NUMBER}.png", bbox_inches='tight')
+    print(x_opt)
 
 
 if __name__=="__main__":
